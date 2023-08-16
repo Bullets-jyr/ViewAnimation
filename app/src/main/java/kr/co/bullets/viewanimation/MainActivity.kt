e469152588a78d837f8024003e91580b116b33c6
Bullets-jyr <jyr@bullets.co.kr>
2023-08-17 00:25:03 +0900

    6. Create Animation In Code

--- app/src/main/java/kr/co/bullets/viewanimation/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/viewanimation/MainActivity.kt
@@ -2,10 +2,48 @@ package kr.co.bullets.viewanimation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import android.view.animation.AnimationSet
+import android.view.animation.RotateAnimation
+import android.view.animation.ScaleAnimation
+import android.view.animation.TranslateAnimation
+import kr.co.bullets.viewanimation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.start.setOnClickListener { button ->
+            val myAnimationSet = AnimationSet(true)
+            myAnimationSet.duration = 3000
+            val translate = TranslateAnimation(
+                0f,
+                300f,
+                0f,
+                300f
+            )
+
+            val rotate = RotateAnimation(0f, 360f)
+            val alpha = AlphaAnimation(1f, 0f)
+            val scale = ScaleAnimation(1f, 3f, 1f, 3f)
+//            myAnimationSet.addAnimation(translate)
+//            myAnimationSet.addAnimation(rotate)
+//            myAnimationSet.addAnimation(alpha)
+//            myAnimationSet.addAnimation(scale)
+
+            myAnimationSet.apply {
+                addAnimation(translate)
+                addAnimation(rotate)
+                addAnimation(alpha)
+                addAnimation(scale)
+            }
+
+            button.startAnimation(myAnimationSet)
+        }
     }
 }
